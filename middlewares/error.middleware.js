97b464d1df18fe213ae87fe451cf64811ef36d3b
Aritra <aritrasarkar00007@gmail.com>
2025-02-12 22:00:16 +0530

    error handling

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -5,9 +5,32 @@ const errorMiddleware = (err,req,res,next)=>{
         error.message = err.message;
         console.error(err);
         
+        if(err.name ==='CastError'){
+            const message = 'Resource not found';
+
+            error = new Error(message);
+            error.statusCode = 404;
+        }
+
+        //mongoose duplicate key
+        if(err.code ===11000){
+            const message = 'Duplicate key';
+            error = new Error(message);
+            error.statusCode = 400;
+        }
+
+        //mongoose validation error
+        if(err.name ==='ValidationError'){
+            const message = Object.values(err.errors).map(val => val.message);
+            error = new Error(message.join(', '));
+            error.statusCode = 400;
+        }
+
+        res.status(error.statusCode || 500).json({success: false,error: error.message  || "Internal server error ;)"});
     }
     catch(error){
         next(error);
     }
 }
 
+export default errorMiddleware;
