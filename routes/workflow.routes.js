b1c50a7844e2582e6365e8639043cf92678a4b8e
Aritra <aritrasarkar00007@gmail.com>
2025-02-17 21:15:19 +0530

    email reminders implemented

--- routes/workflow.routes.js
+++ routes/workflow.routes.js
@@ -1,8 +1,8 @@
-import { Router } from "express";
+import { Router} from 'express';
+import { sendReminders } from '../controllers/workflow.controller.js'
 
+const workflowRouter = Router();
 
-const workflowRouter = Router():
-
-workflowRouter.get('/',(req,res) =>{});
+workflowRouter.post('/subscription/reminder', sendReminders);
 
 export default workflowRouter;
